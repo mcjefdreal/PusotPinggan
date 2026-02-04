9b0e93697a7fd4e2e0872e7d257755f762ad480e
Michael Real <michaeldreal15@gmail.com>
2026-02-04 18:32:06 +0800

    feat(navbar): add dot to active page

--- app/src/lib/components/Navbar/Navbar.svelte
+++ app/src/lib/components/Navbar/Navbar.svelte
@@ -14,21 +14,33 @@
         <li>
             <a class="flex h-full items-center" href="{resolve('/')}">
                 <StoreIcon active={page.url.pathname === '/'}/>
+                {#if page.url.pathname === '/'}
+                    <div class="absolute -bottom-1 h-1 w-1 rounded-full bg-pp-pink"></div>
+                {/if}
             </a>
         </li>
         <li>
-            <a class="flex h-full items-center" href="{resolve('/home/')}">
+            <a class="relative flex flex-col h-full items-center" href="{resolve('/home/')}">
                 <HomeIcon active={page.url.pathname === '/home'}/>
+                {#if page.url.pathname === '/home'}
+                    <div class="absolute -bottom-1 h-1 w-1 rounded-full bg-pp-pink"></div>
+                {/if}
             </a>
         </li>
         <li>
             <a class="flex h-full items-center" href="{resolve('/')}">
                 <MsgIcon active={page.url.pathname === '/'}/>
+                {#if page.url.pathname === '/'}
+                    <div class="absolute -bottom-1 h-1 w-1 rounded-full bg-pp-pink"></div>
+                {/if}
             </a>
         </li>
         <li>
             <a class="flex h-full items-center" href="{resolve('/')}">
                 <ProfileIcon active={page.url.pathname === '/'}/>
+                {#if page.url.pathname === '/'}
+                    <div class="absolute -bottom-1 h-1 w-1 rounded-full bg-pp-pink"></div>
+                {/if}
             </a>
         </li>
     </ul>
