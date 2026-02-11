e4164a2ab581570386c67fe21301e082f47af405
Michael Real <michaeldreal15@gmail.com>
2026-02-11 10:11:06 +0800

    chore: adjust test name

--- app/tests/root.test.ts
+++ app/tests/root.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Should display the home page (not logged in)', async({ page }) => {
+test('Should display the landing page of an unauthorized user', async({ page }) => {
     await page.goto('/');
     await expect(
         page.getByRole('heading', {name: "Puso't Pinggan"}),
