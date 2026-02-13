18a1ecfda701345ce797d59b5ac263a3a7203bf7
Michael Real <michaeldreal15@gmail.com>
2026-02-13 15:26:13 +0800

    feat: add google oauth

---
+++ app/src/routes/(pages)/+layout.server.ts
@@ -0,0 +1,15 @@
+import { redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types.js';
+
+export const load: PageServerLoad = async ({ locals }) => {
+  const { session } = await locals.safeGetSession();
+
+  // If no session cookie exists, send them back to login
+  if (!session) {
+    throw redirect(303, '/');
+  }
+
+  return {
+    user: session.user
+  };
+};
