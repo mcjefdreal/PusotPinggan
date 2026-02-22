8bad482ee3eeb6ca629306f12a8d48402d9a9350
kalatkarin <carlynecconsebido@gmail.com>
2026-02-22 17:39:05 +0800

    feat: added store info and relevant components

---
+++ app/src/lib/components/CreateStore/StorePreviewCard.svelte
@@ -0,0 +1,29 @@
+<script>
+    const {
+        storePicUrl,
+        storeName,
+    } = $props();
+
+</script>
+
+<div class="flex flex-row">
+    <img src={storePicUrl} alt={storeName} class="w-50 h-50 rounded-lg" />
+
+    <div class="flex flex-col">
+        <p> {storeName} </p>
+
+        <div class="flex flex-row"> </div>
+        <a
+			class="bg-pp-pink text-pp-white flex h-10 w-40 items-center justify-center rounded-lg text-lg font-medium"
+			href='/'
+		>
+			View Store
+		</a>
+        <a
+			class="bg-pp-pink text-pp-white flex h-10 w-40 items-center justify-center rounded-lg text-lg font-medium"
+			href='/'
+		>
+			View Orders
+		</a>
+    </div>
+</div>
