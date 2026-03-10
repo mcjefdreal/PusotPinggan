c5372809082294573cd897840faa4614ca8f8878
kalatkarin <carlynecconsebido@gmail.com>
2026-03-10 19:14:54 +0800

    feat: created ChatPreview component

---
+++ app/src/lib/components/Messages/ChatPreview.svelte
@@ -0,0 +1,21 @@
+<script lang=ts>
+    let {
+		// userPic,
+		userName = 'John Pork',
+		latestMsg = 'heyyyyyyy',
+        timestamp = '7m',
+        read = false
+	} = $props();
+</script>
+
+<div class="flex flex-row">
+    <div class="bg-pp-light-pink h-20 w-20 rounded-full"> </div>
+    <div class="flex flex-col justify-center gap-1 pl-4">
+        <p class="text-lg font-semibold"> {userName} </p>
+        <div class="flex flex-row gap-4 {read ? 'text-pp-gray' : 'text-pp-black'}"> 
+            <p> {latestMsg} </p>
+            <p> · </p>
+            <p> {timestamp} </p>
+        </div>
+    </div>
+</div>
