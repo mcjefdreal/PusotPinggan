18e44a71b8ca4854aad70e21dce9183e22db4edd
kalatkarin <carlynecconsebido@gmail.com>
2026-04-03 10:12:18 +0800

    feat: differentiated products and stores in search results

--- app/src/routes/(pages)/search/+page.svelte
+++ app/src/routes/(pages)/search/+page.svelte
@@ -18,21 +18,37 @@
 		</div>
 	{:else}
 		<div class="px-4 pb-24">
+			<!-- display stores first -->
+			{#if data.stores.length != 0}
+				<p class="pb-3 text-xl font-medium"> Stores </p>
+			{/if}
+			{#each data.stores as s (s.store_id)}
+				<div class="flex flex-row items-start pb-5">
+					<img src={s.img_url} alt={s.store_name} class="h-35 w-35 rounded-lg" />
+					<div class="flex flex-col overflow-hidden px-5">
+						<p class="text-pp-pink truncate pb-5 text-2xl font-semibold">{s.store_name}</p>
+						<a
+							class="bg-pp-pink text-pp-white my-1 flex h-8 w-25 items-center justify-center rounded-lg text-sm"
+							href="/"
+							data-testid="view-store"
+						>
+							View Store
+						</a>
+					</div>
+				</div>
+			{/each}
+
+			{#if data.products.length != 0}
+				<p class="py-3 text-xl font-medium"> Products </p>
+			{/if}
 			<div class="grid grid-cols-2 gap-3">
+				<!-- display products second -->
 				{#each data.products as p (p.product_id)}
 					<div class="border-pp-gray/30 overflow-hidden rounded-xl border">
 						<img class="h-28 w-full object-cover" src={p.img_url} alt={p.name} />
 						<div class="p-2">
 							<div class="text-pp-black text-sm font-semibold">{p.name}</div>
 							<div class="text-pp-gray text-xs">₱ {p.price.toFixed(2)}</div>
-						</div>
-					</div>
-				{/each}
-				{#each data.stores as s (s.store_id)}
-					<div class="border-pp-gray/30 overflow-hidden rounded-xl border">
-						<img class="h-28 w-full object-cover" src={s.img_url} alt={s.store_name} />
-						<div class="p-2">
-							<div class="text-pp-black text-sm font-semibold">{s.store_name}</div>
 						</div>
 					</div>
 				{/each}
