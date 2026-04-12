5c802ddd5f477492f941b3cc113db84b1290dddf
Michael Real <michaeldreal15@gmail.com>
2026-04-13 04:08:46 +0800

    fix: scrolling issue

--- app/src/routes/(pages)/profile/basket/+layout.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	let { data, children } = $props();
-</script>
-
-{@render children()}
---
+++ app/src/routes/(pages)/profile/basket/+layout@.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Navbar from '$lib/components/Navbar/Navbar.svelte';
+	const { children } = $props();
+</script>
+
+<div class="bg-pp-white flex h-screen flex-col overflow-y-auto">
+	<main class="flex-1 overflow-y-auto">
+		{@render children()}
+	</main>
+	<Navbar />
+</div>
