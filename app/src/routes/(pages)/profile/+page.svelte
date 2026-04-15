ad59d6b2a048a3d80d6b327ada0844bbbfb423c9
truenofalse <ntzuniga04@gmail.com>
2026-04-16 00:22:33 +0800

    chore: run linters and formatters

--- app/src/routes/(pages)/profile/+page.svelte
+++ app/src/routes/(pages)/profile/+page.svelte
@@ -28,12 +28,12 @@
 	}
 </script>
 
-<div class="min-h-screen flex flex-col">
-	<div class=" bg-gradient-to-t from-pp-pink to-pp-light-pink px-6 pt-6 pb-20 p-6">
+<div class="flex min-h-screen flex-col">
+	<div class=" from-pp-pink to-pp-light-pink bg-gradient-to-t p-6 px-6 pt-6 pb-20">
 		<div class="text-pp-white text-center text-2xl font-semibold">Profile</div>
 		<div class="mt-4 flex flex-col items-center justify-center">
 			<div
-				class="flex h-36 w-36 items-center justify-center rounded-full border-4 border-white bg-white overflow-hidden"
+				class="flex h-36 w-36 items-center justify-center overflow-hidden rounded-full border-4 border-white bg-white"
 			>
 				<img src={imgUrl} alt="Profile" class="h-full w-full object-cover" />
 			</div>
@@ -44,32 +44,32 @@
 	</div>
 
 	<div class="flex-1 bg-rose-50">
-		<div class="flex-1 bg-pp-white mx-auto w-full max-w-[85%] rounded-xl -mt-15">
-		<a
-			href={resolve('/profile/edit')}
-			class="block flex w-full items-center rounded-t-xl px-3 py-4 text-base font-normal transition hover:bg-gray-100 active:bg-gray-300"
-		>
-			<FilePenOutline class="mr-4 ml-2 inline-block h-5 w-5" />
-			Edit Profile
-			<AngleRightOutline class="mr-1 ml-auto h-5 w-5" />
-		</a>
-		<div class="border-t-1 border-dotted border-gray-300"></div>
-		<a
-			href="/profile/basket/cart"
-			class="block flex w-full items-center px-3 py-4 text-base font-normal transition hover:bg-gray-100 active:bg-gray-300"
-		>
-			<CartOutline class="mr-4 ml-2 inline-block h-5 w-5" />
-			View Basket
-			<AngleRightOutline class="mr-1 ml-auto h-5 w-5" />
-		</a>
-		<div class="border-t-1 border-dotted border-gray-300"></div>
-		<button
-			class="block w-full rounded-b-xl px-3 py-4 text-left text-base font-normal transition hover:bg-gray-100 active:bg-gray-300"
-			onclick={signOut}
-		>
-			<ArrowRightToBracketOutline class="mr-3 ml-2 inline-block h-5 w-5 text-red-800" />
-			Log Out
-		</button>
+		<div class="bg-pp-white mx-auto -mt-15 w-full max-w-[85%] flex-1 rounded-xl">
+			<a
+				href={resolve('/profile/edit')}
+				class="block flex w-full items-center rounded-t-xl px-3 py-4 text-base font-normal transition hover:bg-gray-100 active:bg-gray-300"
+			>
+				<FilePenOutline class="mr-4 ml-2 inline-block h-5 w-5" />
+				Edit Profile
+				<AngleRightOutline class="mr-1 ml-auto h-5 w-5" />
+			</a>
+			<div class="border-t-1 border-dotted border-gray-300"></div>
+			<a
+				href="/profile/basket/cart"
+				class="block flex w-full items-center px-3 py-4 text-base font-normal transition hover:bg-gray-100 active:bg-gray-300"
+			>
+				<CartOutline class="mr-4 ml-2 inline-block h-5 w-5" />
+				View Basket
+				<AngleRightOutline class="mr-1 ml-auto h-5 w-5" />
+			</a>
+			<div class="border-t-1 border-dotted border-gray-300"></div>
+			<button
+				class="block w-full rounded-b-xl px-3 py-4 text-left text-base font-normal transition hover:bg-gray-100 active:bg-gray-300"
+				onclick={signOut}
+			>
+				<ArrowRightToBracketOutline class="mr-3 ml-2 inline-block h-5 w-5 text-red-800" />
+				Log Out
+			</button>
 		</div>
 	</div>
 </div>
