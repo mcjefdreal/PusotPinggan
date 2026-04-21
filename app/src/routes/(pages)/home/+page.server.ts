2cebbb4555845a00a482256e48bbef64080399e1
Michael Real <michaeldreal15@gmail.com>
2026-04-21 10:57:45 +0800

    feat: optimize home load function

--- app/src/routes/(pages)/home/+page.server.ts
+++ app/src/routes/(pages)/home/+page.server.ts
@@ -10,21 +10,11 @@ export const load: PageServerLoad = async ({ parent, locals: { supabase } }) => 
 
 	const userId = user.id;
 
-	// Fetch all stores
-	const { data: stores, error: storesError } = await supabase.from('store').select('*');
+	const { data: stores, error: storesError } = await supabase.rpc('get_stores_with_distances', {
+	p_user_id: userId
+	});
 
 	if (storesError) throw error(500, storesError.message);
-
-	// Calculate distance for each store
-	const storesWithDistance = await Promise.all(
-		stores.map(async (store) => {
-			const { data: distance } = await supabase.rpc('get_store_distance', {
-				p_user_id: userId,
-				p_store_id: store.store_id
-			});
-			return { ...store, distance_meters: distance || 0 };
-		})
-	);
 
 	// Fetch products where quantity > 0, joined with store info
 	const { data: products, error: productsError } = await supabase
@@ -44,7 +34,7 @@ export const load: PageServerLoad = async ({ parent, locals: { supabase } }) => 
 	if (productsError) throw error(500, productsError.message);
 
 	return {
-		stores: storesWithDistance,
+		stores,
 		products
 	};
 };
