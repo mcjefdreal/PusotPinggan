1ca2170d3448d532bae7725838e182487887eb25
kalatkarin <carlynecconsebido@gmail.com>
2026-04-23 16:00:55 +0800

    feat: added store hours view

---
+++ app/src/lib/components/Store/StoreHoursModal.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { Modal } from 'flowbite-svelte';
+
+	let {
+		editModal = false,
+		onClose,
+		storeHrs = {},
+        isOpen
+	} = $props();
+
+	type DaySchedule = { open: string; close: string };
+	type WeekSchedule = Record<string, DaySchedule>;
+
+	let schedule: WeekSchedule = $derived({
+		monday: storeHrs?.monday || { open: '00:00', close: '00:00' },
+		tuesday: storeHrs?.tuesday || { open: '00:00', close: '00:00' },
+		wednesday: storeHrs?.wednesday || { open: '00:00', close: '00:00' },
+		thursday: storeHrs?.thursday || { open: '00:00', close: '00:00' },
+		friday: storeHrs?.friday || { open: '00:00', close: '00:00' },
+		saturday: storeHrs?.saturday || { open: '00:00', close: '00:00' },
+		sunday: storeHrs?.sunday || { open: '00:00', close: '00:00' }
+	});
+
+    let today = $derived(new Date().toLocaleDateString('en-US', { weekday: 'long' }).toLowerCase());
+</script>
+
+<Modal bind:open={editModal} class="max-w-100" onclose={onClose}>
+    <p class="text-xl text-pp-black font-medium"> Store Hours </p>
+    {#if isOpen} 
+        <p class="text-green-700"> Open Now </p>
+    {:else}
+        <p class="text-red-500"> Currently Closed </p>
+    {/if}
+    <div class="grid grid-cols-2">
+        {#each Object.keys(schedule) as day (day)}
+            <div class="capitalize {today === day ? 'font-bold' : 'font-medium'}"> {day} </div>
+            <div class="{today === day ? 'font-bold' : 'font-normal'}"> {schedule[day].open} - {schedule[day].close} </div>
+        {/each}
+    </div>
+</Modal>
--- app/src/routes/(pages)/search/[storename]/[id]/+page.svelte
+++ app/src/routes/(pages)/search/[storename]/[id]/+page.svelte
@@ -1,14 +1,61 @@
 <script lang="ts">
 	import type { PageProps } from './$types.ts';
-	import { ArrowLeftOutline } from 'flowbite-svelte-icons';
+	import { ArrowLeftOutline, ClockOutline } from 'flowbite-svelte-icons';
 
 	import { resolve } from '$app/paths';
 
 	import StoreProductCard from '$lib/components/Homepage/StoreProductCard.svelte';
+	import StoreHoursModal from '$lib/components/Store/StoreHoursModal.svelte';
 
 	let { data }: PageProps = $props();
 
 	let products = $derived(data.products);
+
+	let storeHoursModal = $state(false);
+
+	function handleClose() {
+		storeHoursModal = false;
+	}
+
+	type DaySchedule = { open: string; close: string };
+
+	function isStoreOpen(storeHrs: Record<string, DaySchedule>): boolean {
+        const now = new Date();
+
+        // check day first
+        const day = now.toLocaleDateString('en-US', { weekday: 'long' }).toLowerCase();
+
+        const todaySchedule = storeHrs?.[day];
+        if (!todaySchedule) return false; // lowk useless rn pero for future use<3
+
+        const { open, close } = todaySchedule;
+
+        // convert everything to minutes
+        // day -> 0 minutes to 1440 minutes
+        // say open = 8am, close = 6pm
+        // -> [480, 1080]
+        // say current time is 3:20 pm -> 900+20 = 920 minutes (within range -> open!)
+
+        const nowMinutes = now.getHours() * 60 + now.getMinutes();
+
+        const toMinutes = (time: string) => {
+            const [h, m] = time.split(':').map(Number);
+            return h * 60 + m;
+        };
+
+        const openMinutes = toMinutes(open);
+        const closeMinutes = toMinutes(close);
+		
+        if (openMinutes <= nowMinutes && nowMinutes < closeMinutes){
+            return true;
+        }
+
+        else {
+            return false;
+        }
+    }
+
+    let isOpen = $derived(isStoreOpen(data.store.store_hrs));
 </script>
 
 <div class="min-h-screen w-full">
@@ -36,7 +83,20 @@
 			<div class="text-pp-pink text-xl font-semibold">{data.store.store_name}</div>
 			<div class="text-pp-gray text-xs">⭐ {data.store.rating}</div>
 			<p class="text-pp-gray mt-2 text-xs">{data.store.store_desc}</p>
+			
+			<div class="flex flex-row pt-3">
+				<button onclick={() => (storeHoursModal = true)}> 
+					<ClockOutline class="inline"/> 
+					{#if isOpen} 
+						<p class="inline text-green-700"> Open Now </p>
+					{:else}
+						<p class="inline text-red-500"> Currently Closed </p>
+					{/if}
+				</button>
+			</div>
 		</div>
+
+		<hr class="border-pp-gray pb-3">
 
 		<!-- Products -->
 		<div class="px-4 pb-24">
@@ -55,4 +115,12 @@
 			</div>
 		</div>
 	</div>
+
+	<StoreHoursModal
+		editModal={storeHoursModal}
+		onClose={handleClose}
+		storeHrs={data.store.store_hrs}
+		isOpen={isOpen}
+	/>
 </div>
+
